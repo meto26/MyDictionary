2057dcb8750b577a75659f7ea2ceedd3aa7534ef
Metehan Çavdar <metehan262626@gmail.com>
2021-03-01 13:07:42 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Mete");
+            myDictionary.Add(2, "Nazmi");
+            myDictionary.Add(3, "Engin");
+            myDictionary.Add(4, "Batur");
+            myDictionary.Add(5, "Bilge");
+            myDictionary.Add(6, "Berk");
+
+            myDictionary.ToList();
+        }
+    }
+}
